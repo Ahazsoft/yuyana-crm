95155ab1175a91a43718333f50c3e73286dc1d23
Eyuel Solomon <122565078+Fine-Guy-21@users.noreply.github.com>
2026-04-03 18:02:28 +0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   datasource: {
-    // url: env("DATABASE_URL"),
+    url: env("DATABASE_URL"),
 
-    url: env("DIRECT_URL"),
+    // url: env("DIRECT_URL"),
   },
   migrations: {
     seed: "npx tsx prisma/seeds/full-seed.ts",
