0527ac08baecd246bfe886cabe5ee1d1e5a0efa2
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2026-05-15 17:18:06 +0300

    feat: cron jobs added

--- actions/get-modules.ts
+++ actions/get-modules.ts
@@ -4,6 +4,5 @@ export const getModules = async () => {
   const data = await prismadb.system_Modules_Enabled.findMany({
     orderBy: [{ position: "asc" }],
   });
-  console.log("Modules data:", data); // Debug log to check the structure of the data
   return data;
 };
